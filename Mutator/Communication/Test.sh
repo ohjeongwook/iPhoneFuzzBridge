4d65da44f66a5b1be1510655713a1443c0e59551
jeongwook oh <jeongwookoh@matt-mac.websense.com>
2010-08-17 12:38:40 -0700

    Added pdf test case

--- Mutator/Communication/Test.sh
+++ Mutator/Communication/Test.sh
@@ -1,5 +1,7 @@
-python ControlAgent.py iphone 1234 http://www.yahoo.com
-python ControlAgent.py iphone 1234 http://www.bing.com
+python ControlAgent.py iphone 1234 http://10.211.1.90/out/iPad1,1_3.2.1.pdf
+#http://www.jailbreakme.com/_/iPad1%2c1_3.2.1.pdf
+#python ControlAgent.py iphone 1234 http://www.yahoo.com
+#python ControlAgent.py iphone 1234 http://www.bing.com
 #python ControlAgent.py iphone 1234 ../Samples/Icon.png
 #python ControlAgent.py iphone 1234 ../Samples/Grand_Central_test.jpg
 #python ControlAgent.py iphone 1234 ../Samples/pst.wav
